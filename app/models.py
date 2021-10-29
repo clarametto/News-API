3f7ac55281cb720d660bf71bd329dddac733094a
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-10-30 01:13:30 +0300

    created news model

---
+++ app/models.py
@@ -0,0 +1,12 @@
+class News:
+    def __init__(self, id, name, description, url, category, language, country):
+        self.id=id
+        self.name=name 
+        self.description=description
+        self.url=url
+        self.category=category
+        self.country=country
+        self.language=language
+        
+
+        
