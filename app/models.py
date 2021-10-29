13fa389f0c46161da74ed00a261c062decf73564
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-10-30 01:14:48 +0300

    created articles model

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 class News:
-    def __init__(self, id, name, description, url, category, language, country):
+    def __init__(self, id, name, description, url, category, country, language):
         self.id=id
         self.name=name 
         self.description=description
@@ -9,4 +9,14 @@ class News:
         self.language=language
         
 
+
+class Articles:
+    def __init__(self, id, title, author, description, url, urlToImage, publishAt):
+        self.id=id
+        self.title=title
+        self.author=author
+        self.description=description
+        self.url=url
+        self.urlToImage=urlToImage
+        self.publishAt=publishAt
         
